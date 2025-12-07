64a5fb4f902e272f40506e5d060659b72b83eb5d
Chris Birster <christopher.birster@gmail.com>
2025-12-07 13:38:55 -0500

    create basic task

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,6 @@ const donegeon = @import("donegeon");
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try donegeon.bufferedPrint();
     const result = donegeon.add(13, 29);
     std.debug.print("13 + 29 = {d}\n", .{result});
 }
@@ -15,15 +14,4 @@ test "simple test" {
     defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
     try list.append(gpa, 42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
--- src/root.zig
+++ src/root.zig
@@ -1,17 +1,23 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+const Task = struct {
+    name: []const u8,
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    pub fn init(name: []const u8) Task {
+        return Task{ .name = name };
+    }
+};
 
-    try stdout.flush(); // Don't forget to flush!
+pub fn createTask(name: []const u8) Task {
+    const task = Task.init(name);
+    return task;
+}
+
+test "create task" {
+    const name = "hi mom";
+    const task = createTask(name);
+    try std.testing.expectEqualStrings("hi mom", task.name);
 }
 
 pub fn add(a: i32, b: i32) i32 {
