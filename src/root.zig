414181957f1144662d74ccfbffaad3868cc3fe53
Chris Birster <christopher.birster@gmail.com>
2025-12-07 13:45:52 -0500

    add optional desc

--- src/root.zig
+++ src/root.zig
@@ -3,21 +3,30 @@ const std = @import("std");
 
 const Task = struct {
     name: []const u8,
+    description: ?[]const u8,
 
-    pub fn init(name: []const u8) Task {
-        return Task{ .name = name };
+    pub fn init(name: []const u8, description: ?[]const u8) Task {
+        return Task{ .name = name, .description = description };
     }
 };
 
-pub fn createTask(name: []const u8) Task {
-    const task = Task.init(name);
+pub fn createTask(name: []const u8, description: ?[]const u8) Task {
+    const task = Task.init(name, description);
     return task;
 }
 
-test "create task" {
+test "create task with name" {
     const name = "hi mom";
-    const task = createTask(name);
+    const task = createTask(name, null);
     try std.testing.expectEqualStrings("hi mom", task.name);
+}
+
+test "create task with optional description" {
+    const name = "hi mom";
+    const description = "this worked";
+    const task = createTask(name, description);
+    try std.testing.expectEqualStrings("hi mom", task.name);
+    try std.testing.expectEqualStrings("this worked", task.description.?);
 }
 
 pub fn add(a: i32, b: i32) i32 {
